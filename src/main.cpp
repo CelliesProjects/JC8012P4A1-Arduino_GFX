a88e78c3b2112d742af26b4fd8554e22f9429305
Cellie <CelliesProjects@users.noreply.github.com>
2025-09-01 19:58:06 +0200

    Cleanup

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,7 @@
 //#include <WiFi.h>
 
 #include <Arduino_GFX_Library.h>
+#include "Arduino_GFX_dev_device.h"
 
 #include "touch/gsl3680_touch.h"
 #include "touch/esp_lcd_touch.h"
@@ -13,27 +14,7 @@
 
 const int rotation = 0;
 
-/// see https://github.com/moononournation/Arduino_GFX/blob/master/examples/PDQgraphicstest/Arduino_GFX_dev_device.h
-
 // https://docs.espressif.com/projects/esp-iot-solution/en/latest/display/lcd/lcd_screen_tearing.html
-
-Arduino_ESP32DSIPanel *dsipanel = new Arduino_ESP32DSIPanel(
-    20 /* hsync_pulse_width */,
-    20 /* hsync_back_porch */,
-    40 /* hsync_front_porch */,
-    4 /* vsync_pulse_width */,
-    8 /*vsync_back_porch  */,
-    20 /* vsync_front_porch */,
-    60000000 /* prefer_speed */);
-
-Arduino_DSI_Display *gfx = new Arduino_DSI_Display(
-    800 /* width */,
-    1280 /* height */,
-    dsipanel,
-    rotation /* rotation */,
-    true /* auto_flush */,
-    27 /* RST */,
-    jd9365_init_operations, sizeof(jd9365_init_operations) / sizeof(lcd_init_cmd_t));
 
 gsl3680_touch touch(TP_I2C_SDA, TP_I2C_SCL, TP_RST, TP_INT);
 
@@ -49,7 +30,7 @@ void setup()
             delay(1);
     }
 
-    log_i("Display initialized");
+    Serial.println("Display initialized");
 
     gfx->setCursor(10, 10);
     gfx->setFont();
@@ -59,29 +40,28 @@ void setup()
     touch.begin();
     touch.set_rotation(rotation);
 
-    constexpr int BACKLIGHT_PIN = 23;
     constexpr int BIT_DEPTH = 14;
     constexpr int MAX_PWM_VAL = 1 << BIT_DEPTH;
-    if (!ledcAttachChannel(BACKLIGHT_PIN, 1220, BIT_DEPTH, 0))
+    if (!ledcAttachChannel(GFX_BL, 1220, BIT_DEPTH, 0))
     {
-        log_e("Error setting ledc pin %i. system halted", index);
+        Serial.printf("Error setting ledc pin %i. system halted", index);
         while (1)
             delay(1000);
     }
 
-    if (!ledcWrite(BACKLIGHT_PIN, MAX_PWM_VAL >> 3))
+    if (!ledcWrite(GFX_BL, MAX_PWM_VAL >> 3))
     {
-        log_e("Error setting ledc value. system halted");
+        Serial.printf("Error setting ledc value. system halted");
         while (1)
             delay(1000);
     }
 
-    //log_i("WiFi waiting for %s", SSID);
+    //Serial.printf("WiFi waiting for %s\n", SSID);
 
     // while (!WiFi.isConnected())
     // delay(10);
 
-    log_i("entering loop");
+    Serial.println("entering loop");
 }
 
 void loop()
